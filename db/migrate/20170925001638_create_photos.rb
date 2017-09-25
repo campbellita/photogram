4ae16da0c15cc387520c8dd5edf9daac1d54ce81
Raghu Betina <raghu@firstdraft.com>
2017-09-25 00:16:38 +0000

    Generate photos resource with starter:resource

---
+++ db/migrate/20170925001638_create_photos.rb
@@ -0,0 +1,12 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.text :description
+      t.integer :user_id
+      t.string :image
+
+      t.timestamps
+
+    end
+  end
+end
